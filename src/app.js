3eedaefdd222a1c9497f28644d29e070aa5d0962
belal sryo <belalsryo@gmail.com>
2026-02-16 20:49:43 +0200

    feat: add AppError class for centralized error handling and update app.js to utilize it

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import passport from './config/passport.js';
 import problemRouter from './modules/problem/problem.routes.js';
 import userRouter from './modules/user/user.routes.js';
 import authRouter from './modules/auth/auth.routes.js';
+import AppError from './utils/AppError.js';
 
 const app = express();
 
@@ -19,6 +20,18 @@ app.get('/test', (req, res) => {
   res.status(200).json({
     status: 'success',
     message: 'Hello express :)'
+  });
+});
+
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(err.statusCode || 500).json({
+    status: err.status || 'error',
+    message: err.message || 'Internal Server Error'
   });
 });
 
