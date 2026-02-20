6de5c907281939de63224cfa16834140a19dc4b4
belal sryo <belalsryo@gmail.com>
2026-02-20 19:43:53 +0200

    feat: enhance getProblems function with query handling and improve error handling in getProblemBySlug, updateProblem, and deleteProblem

--- src/modules/problem/problem.services.js
+++ src/modules/problem/problem.services.js
@@ -1,17 +1,28 @@
 import Problem from './problem.model.js';
+import AppError from '../../utils/AppError.js';
+import Query from '../../utils/query.js';
 
 export const createProblem = async (problemData) => {
   const problem = await Problem.create(problemData);
   return problem;
 };
 
-export const getProblems = async () => {
-  const problems = await Problem.find();
+export const getProblems = async (queryString) => {
+  const query = new Query(Problem.find(), queryString)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const problems = await query.query;
   return problems;
 };
 
 export const getProblemBySlug = async (slug) => {
   const problem = await Problem.findOne({ slug });
+  if (!problem) {
+    throw new AppError('Problem not found', 404);
+  }
   return problem;
 };
 
@@ -20,10 +31,16 @@ export const updateProblem = async (slug, updateData) => {
     new: true,
     runValidators: true
   });
+  if (!problem) {
+    throw new AppError('Problem not found', 404);
+  }
   return problem;
 };
 
 export const deleteProblem = async (slug) => {
   const problem = await Problem.findOneAndDelete({ slug });
+  if (!problem) {
+    throw new AppError('Problem not found', 404);
+  }
   return problem;
 };
