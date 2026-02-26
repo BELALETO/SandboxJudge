f56f1a7a7f7688c58559cffd81b59487cb56bdd0
belal sryo <belalsryo@gmail.com>
2026-02-26 15:49:35 +0200

    feat: refactor database connection functions to use catchAsync for error handling

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,16 @@
 import mongoose from 'mongoose';
 import config from './config.js';
+import { catchAsync } from '../utils/catchAsync.js';
 
 const { mongoURI, mongoURIProd } = config;
 
-export async function connectDB() {
-  try {
-    await mongoose.connect(mongoURIProd);
-    console.log('Database connected');
-  } catch (error) {
-    console.error(error);
-  }
-}
+export const connectDB = catchAsync(async () => {
+  const uri = process.env.ENV === 'production' ? mongoURIProd : mongoURI;
+  await mongoose.connect(uri);
+  console.log('Database connected');
+});
 
-export async function disconnectDB() {
+export const disconnectDB = catchAsync(async () => {
   await mongoose.disconnect();
   console.log('Database disconnected');
-}
+});
