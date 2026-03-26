032a28f3a9148daee03c76eff99aa17cbd259c3a
belal sryo <belalsryo@gmail.com>
2026-03-26 02:30:09 +0200

    feat: Add Swagger API documentation and integrate Helmet for enhanced security.

--- src/app.js
+++ src/app.js
@@ -7,10 +7,20 @@ import submissionRouter from './modules/submission/submission.routes.js';
 import AppError from './utils/AppError.js';
 import errorHandler from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
 const app = express();
 
 app.set('query parser', 'extended');
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('dev'));
@@ -19,6 +29,8 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/problems', problemRouter);
 app.use('/api/v1/submit', submissionRouter);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //TODO: A test route to run code without logging in.
 
